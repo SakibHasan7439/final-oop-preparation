e99f65fe34fc74a7d3da99f5c3529f19221cd08b
sakibHasan7439 <sakibhasanb120@gmail.com>
2025-06-19 15:20:40 +0600

    first commit

---
+++ funcOverload.cpp
@@ -0,0 +1,52 @@
+// Function overloading with runtime polymorphism
+#include <iostream>
+using namespace std;
+
+class Animal {
+    public :
+        virtual void speak(){
+            cout <<"Animal speaks.";
+        };
+};
+
+class Dog: public Animal {
+    public:
+        void speak() override {
+            cout <<"Dog Barks!"<<endl;
+        }
+};
+
+class Company {
+    public:
+        virtual void calSalary(){
+
+        };
+};
+
+class Faq: public Company {
+    public:
+        void calSalary(){
+            int working_hour = 5;
+            cout<<"Total salary based on working hour: "<< working_hour * 1000<<endl;
+        };
+};
+
+class Intellect: public Company {
+    public:
+        void calSalary(){
+            int working_hour = 7;
+            cout<<"Total salary in intellect based on working hour: "<< working_hour * 300<<endl;
+        };
+};
+
+int main(){
+    Dog d;
+    d.speak();
+
+    Company * p;
+    Faq F;
+    Intellect I;
+
+    p =& F;
+    F.calSalary();
+};
